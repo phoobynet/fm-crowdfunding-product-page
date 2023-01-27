8e54c28904c438e3a9a0ae9a2d6df3d9b44c0ea4
Glenn <phoobynet@users.noreply.github.com>
2023-01-27 21:21:43 +0000

    wip

--- src/components/BackThisProjectModal.vue
+++ src/components/BackThisProjectModal.vue
@@ -2,19 +2,61 @@
   lang="ts"
   setup
 >
-import { inject } from 'vue'
+import { inject, ref, Ref } from 'vue'
 import { BackThisProjectModalOpen } from '@/lib/injectionKeys'
+import { useMotions } from '@vueuse/motion'
+import { onClickOutside, onKeyStroke } from '@vueuse/core'
 
-const backWithProjectOpen = inject(BackThisProjectModalOpen)
+const emit = defineEmits(['click'])
+const backThisProjectModalOpen = inject(BackThisProjectModalOpen) as Ref<boolean>
+
+const modalElement = ref<HTMLDivElement>()
+
+onClickOutside(modalElement, () => backThisProjectModalOpen.value = false)
+onKeyStroke('Escape', () => {
+  backThisProjectModalOpen.value = false
+})
+
+const motions = useMotions()
 </script>
 
 <template>
-<div></div>
+  <transition
+    :css="false"
+    @leave="(el: Element, done: () => void) => motions.modal.leave(done)"
+  >
+    <div class="back-this-project-modal">
+      <div class="modal">
+        this is a dialog
+      </div>
+    </div>
+  </transition>
 </template>
 
 <style
   lang="scss"
   scoped
 >
+.back-this-project-modal {
+  z-index: 9999;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  // TODO: Adjust height and gradient
+  height: 667px;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  padding-top: 5.5rem;
+  background-size: 100% 667px;
+  background-repeat: no-repeat;
+
+  .modal {
+    background-color: var(--clr-white);
+    border-radius: 0.5rem;
+    width: var(--mob-content-width);
+  }
+}
 </style>
 
